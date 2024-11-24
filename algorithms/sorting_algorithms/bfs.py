3819d6b358c71c0997cb6c44f0248c8c1919c8b3
Berkay <berkaytrhn@gmail.com>
2024-11-24 18:38:31 +0300

    bfs implemented iwth custom callback function using  ds package classes(queue, graph, NonLinearNode, LinearNode),

--- algorithms/sorting_algorithms/bfs.py
+++ algorithms/sorting_algorithms/bfs.py
@@ -1,12 +1,79 @@
-from typing import Any
-from typing import Callable, Dict, List, Tuple, Optional
+from data_structures.non_linear import Graph
+from data_structures.utils.node import NonLinearNode, LinearNode
+from data_structures.linear.queue import Queue
+from typing import Any, Callable, Optional
 
 
 class BFS:
     
-    def __init__(self) -> None:
-        pass
+    def __init__(self, graph: Graph, fields: Optional[Any]=None) -> None:
+        self.graph=graph
+        self.queue: Queue=Queue()
+        self.visited=set()
+        self.fields=fields
     
-    def __call__(self, root) -> Any:
-        pass
+    def traverse(self, value: Any, callback: Callable):
+        """
+        For this implementation, queue nodes are LinearNode,
+        Graph nodes are NonLinearNode,
+        Queue(Simple) implementation uses LinearNode with Any typed input value
+        so, by calling enqueue with NonLinearNode it becomes like one 
+        LinearNode of queue has a value which is a NonLinearNode:
         
+        Queue:
+        front -> LinearNode(
+            value=NonLinearNode(value=Any, ...), 
+            next=intermediate_node, 
+            prev=None
+        )
+        intermediate_node -> LinearNode(
+            value=NonLinearNode(value=Any, ...), 
+            next=rear, 
+            prev=None
+        )
+        rear -> LinearNode(
+            value=NonLinearNode(value=Any, ...), 
+            next=None, 
+            prev=None
+        )
+    
+        """
+        start_node: NonLinearNode = self.graph.get_node(value)
+        
+        
+        # print("start node")
+        # print(start_node)
+        self.queue: Queue=Queue()
+        self.visited=set()
+        self.queue.enqueue(start_node)
+
+        
+        # print(self.queue.rear)
+        # print(self.queue.front)
+        
+        while not self.queue.isEmpty():
+            current_node: LinearNode = self.queue.dequeue()
+            
+            
+            
+            if current_node.value not in self.visited:
+                print(f"Visiting node: {current_node.value}")
+                self.visited.add(current_node.value)
+                
+                
+                # if there is a callback
+                if callback:
+                    callback(self.fields)
+                
+                # current_node: LinearNode
+                # current_node.value:NonLinearNode
+                # neighbour: NonLinearNode
+                # print(f"Loop in neighbours for {current_node}")
+                # print(current_node.value)
+                for neighbour in current_node.value.neighbours:
+                    #print(f"Neighbour: {neighbour}")
+                    
+                    if neighbour not in self.visited:
+                        #print(f"neighbour not in visited: {neighbour}")
+                        self.queue.enqueue(neighbour)
+                    
--- main.py
+++ main.py
@@ -1,3 +1,36 @@
+from data_structures.non_linear.graph import Graph, NonLinearNode
+from algorithms.sorting_algorithms.bfs import BFS
+from typing import Any
+
+graph = Graph(False)
 
 
+graph.add_node(1)
+graph.add_node(2)
+graph.add_node(3)
+graph.add_node(4)
+graph.add_node(5)
 
+graph.add_edge(1,2)
+graph.add_edge(1,3)
+graph.add_edge(2,3)
+graph.add_edge(3,4)
+graph.add_edge(2,4)
+graph.add_edge(4,5)
+graph.add_edge(5,1)
+
+
+graph.display()
+
+
+bfs = BFS(graph, fields={"count": 0})
+visited_nodes = []
+
+def callback_method(fields: Any):
+    print("Callback run!")
+    fields["count"] += 1
+    
+print(graph.get_node(1).neighbours)
+bfs.traverse(1, callback_method)
+
+print(bfs.fields)
